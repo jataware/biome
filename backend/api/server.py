a2f7b772f7e7a10b7b547cd5795a742e7731c277
Joel Quiles <joel@radcoding.com>
2024-04-15 17:25:00 -0400

    Had to add this OPENAI_* env vars to docker-compose for API since shared Settings was requiring it. Added new file/endpoints for api from other UI work

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,22 +1,72 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, exceptions, Request  # HttpException, status
 import logging
 from pydantic import BaseModel
 import os
+import json
 from rq import Queue, Retry
 from redis import Redis
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
 from lib.job_queue import get_job_status
 from lib.gpt_scraper.websource import WebSource
+from lib.api_clients import get_elasticsearch
+from .seed_sources import seed
 
 logger = logging.getLogger(__name__)
-app = FastAPI(docs_url="/")
-
-# TODO copy Dojo's setup that creates es indeces if missing?
 
 redis = Redis(
     host=os.environ.get("REDIS_HOST", "sources_rq_redis.sources"),
     port=int(os.environ.get("REDIS_PORT", 6379)),
 )
 job_queue = Queue(connection=redis, default_timeout=-1)
+
+es_client = get_elasticsearch()
+
+
+def setup_elasticsearch_indexes():
+    """
+    Creates any indexes not present on the connected elasticsearch database.
+    Won't deeply merge or overwrite existing index/properties- only creates
+    missing indeces.
+    Config should match keyword args on:
+    https://elasticsearch-py.readthedocs.io/en/v8.3.2/api.html#elasticsearch.client.IndicesClient.create
+    """
+    indices = {
+        "datasources": {},
+    }
+    for idx, config in indices.items():
+        if not es_client.indices.exists(index=idx):
+            logger.info(f"Creating index {idx}")
+            es_client.indices.create(index=idx, body=config)
+
+    seed(es_client, "datasources")
+
+
+app = FastAPI(docs_url="/")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# @app.exception_handler(exceptions.RequestValidationError)
+# @app.exception_handler(ValidationError)
+# async def validation_exception_handler(request, exc):
+#     logger.info(f"Request or Response validation failed!: {exc}")
+#     exc_json = json.loads(exc.json())
+#     return JSONResponse({"detail": exc_json}, status_code=422)
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    setup_elasticsearch_indexes()
 
 
 @app.get("/status")
@@ -43,3 +93,56 @@ def gpt_scan_uri(payload: list[ScanArguments]):
     # poll here but stream to client as things are ready..
 
     return {"queued": True, "job_id": job.id}
+
+
+def add_id(hit):
+    return {**hit["_source"], "id": hit["_id"]}
+
+def format_source(source_obj):
+    return add_id(source_obj)
+
+
+@app.get("/sources")
+def list_sources():
+    logger.info("Getting all registered sources")
+
+    q = {"query": {"match_all": {}}}
+
+    size = 20  # for now hardcoded
+
+    try:
+        results = es_client.search(index="datasources", body=q, scroll="2m", size=size)
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    totalDocsInPage = len(results["hits"]["hits"])
+
+    logger.info(f"Got {totalDocsInPage} results")
+
+    if totalDocsInPage < size:
+        scroll_id = None
+    else:
+        scroll_id = results.get("_scroll_id", None)
+
+    return {
+        "count": results["hits"]["total"]["value"],
+        "items_in_page": totalDocsInPage,
+        "sources": [format_source(i) for i in results["hits"]["hits"]],
+        "scroll_id": scroll_id,
+    }
+
+
+@app.post("/add-force-source")
+async def add_source_from_payload(request: Request):
+    """
+    Escape hatch to register sources by pushing a known or manual-built json
+    for the datasource instead of scanning the web portal.
+    """
+    json_req = await request.json()
+
+    body = json.dumps(json_req)
+
+    es_client.index(index="datasources", body=body)
+
+    return {"success": True}
--- backend/lib/settings.py
+++ backend/lib/settings.py
@@ -1,17 +1,13 @@
 from pydantic_settings import BaseSettings
 import pprint
-import dotenv
-import os
 
 pp = pprint.PrettyPrinter(indent=2)
-dotenv.load_dotenv()
-
 
 class Settings(BaseSettings):
-    ELASTICSEARCH_URL: str
-    ELASTICSEARCH_PORT: int
+    ELASTICSEARCH_URL: str = "http://localhost"
+    ELASTICSEARCH_PORT: int = 9200
     OPENAI_API_KEY: str
     OPENAI_ORG_ID: str
 
-
 settings = Settings()
+pp.pprint(Settings().model_dump())
