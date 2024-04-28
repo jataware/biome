12977c116e28e164977dd9b68bb1298f5146de07
Joel Quiles <joel@radcoding.com>
2024-04-28 11:38:30 -0400

    Everything working. Added extra properties to manipulate webview(s).

--- recorder/electron.js
+++ recorder/electron.js
@@ -5,6 +5,7 @@ const path = require('node:path');
 
 function handleSetTitle (event, title) {
   const webContents = event.sender;
+  // can also use win from local `win` variable (let win; below)
   const win = BrowserWindow.fromWebContents(webContents);
   win.setTitle(title);
 }
@@ -20,20 +21,56 @@ async function handleFileOpen () {
 let win;
 function createWindow() {
    win = new BrowserWindow({
-    titleBarStyle: 'hidden',
+    titleBarStyle: 'hidden', // TODO does this work in all OS?
+    autoHideMenuBar: true, // TODO test this
+    title: 'Koro Web Recorder',
     width: 1400,
     height: 1000,
+     // https://www.electronjs.org/docs/latest/api/browser-window#new-browserwindowoptions
     webPreferences: {
       // nodeIntegration: true, // NOTE only allowed with ctx iso
+      // nodeIntegrationInSubFrames: false, // enable node in subframes/child wins
+      // preload always has access to node APIs where node int is on/off
       preload: path.join(__dirname, 'preload-local.js'),
+      zoomFactor: 1.0, // default
+      javascript: true,
+      webSecurity: true,// would setting to false allow embedding webview w/o preload
+      // insec content force to true if webSecurity is false:
+      allowRunningInsecureContent: false,
+      images: true, // allow showing images
       // enable webview...
-      webviewTag: true,
-      contextIsolation: false // for getWebContentsId()
+      imageAnimationPolicy: 'animate', // default; can disable GIFs animation etc
+      textAreasAreResizable: true, // default; can disable
+      webgl: true,
+      plugins: false,
+      experimentalFeatures: false,
+      scrollBounce: false, // default; for macos
+      defaultFontSize: 16, // default
+      minimumFontSize: 1, // default=0
+      webviewTag: true, // !important
+      // The Electron API will only be available in the preload script and not the loaded page. This option should be used when loading potentially untrusted remote content to ensure the loaded content cannot tamper with the preload script and any Electron APIs being used:
+      contextIsolation: false, // for getWebContentsId(); comm with preload-view
+      // boolean (optional) - Whether to enable DevTools. If it is set to false, can not use BrowserWindow.webContents.openDevTools() to open DevTools. Default is true.
+      devTools: true,
+      safeDialogs: false, // default; disable consecutive dialogs from websites
+      // if we enable safe dialogs and it triggers:
+      safeDialogsMessage: 'More consecutive dialog creation prevented',
+      disableDialogs: false, // default
+      navigateOnDragDrop: false,
+      spellcheck: false, // default=true
+      // titleBarOverlay // see docs; TODO see if this works in linux/OSX
     },
   });
 
   win.loadFile('index.html');
-  // win.loadURL('http://localhost:3000'); // Load your React app
+  // All web page related events and operations will be done via webcontents:
+  win.webContents.openDevTools();
+
+  // Only works in linux/windows:
+  // win.removeMenu(); // TODO check this out if menu is bothersome
+
+  // Only on macos for traffic light close/min/max icons:
+  // win.setWindowButtonVisibility(true); // could set to false try out
 }
 
 app.whenReady().then(() => {
@@ -75,6 +112,12 @@ app.whenReady().then(() => {
 
   ipcMain.on('nav-recorder', () => {
     win.loadFile('index.html');
+  });
+
+  // Capture screenshot on rect bounds, by node/electron:
+  ipcMain.on('screenshot', (rect) => {
+    const promise = win.capturePage(rect);
+    // TODO use promise and check where image is captured
   });
 
 });
