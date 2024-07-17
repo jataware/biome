ac32ed0b95d32b0bc24c7d0abb300aec8b773f12
Five Grant <5@fivegrant.com>
2024-07-17 10:36:52 -0500

    Fix get_job

--- backend/lib/job_runner/job_runner.py
+++ backend/lib/job_runner/job_runner.py
@@ -6,9 +6,10 @@ from typing import Any
 from uuid import uuid4
 from enum import Enum
 from datetime import datetime
+from time import sleep
 from redis import Redis
 from rq import Queue, Retry, get_current_job
-import rq.job as rq_job
+import rq.job as rq_job_lib
 from rq.exceptions import NoSuchJobError
 
 from lib.settings import settings
@@ -23,7 +24,7 @@ class Job:
     """
     id: str
     session_id: str
-    status: rq_job.JobStatus
+    status: rq_job_lib.JobStatus
     result: Any
     error: str | None
     messages: list[str]
@@ -47,7 +48,7 @@ class JobRunner:
         Initializes job queue and external connections.
         """
         self._redis = Redis(host=settings.REDIS_HOST)
-        self._queue = Queue(connection=self._redis, default_timeout=-1)
+        self._queue_name = 'default'
 
 
     def create_session(self) -> str:
@@ -83,7 +84,7 @@ class JobRunner:
         for job_id in job_ids:
             self._redis.delete(f"messages:{job_id}")
             try:
-                rq_job.Job.fetch(f"{session_id}:{job_id}").delete()
+                rq_job_lib.Job.fetch(f"{session_id}:{job_id}").delete()
             except NoSuchJobError:
                 continue
         self._redis.delete(f"session:{session_id}")
@@ -124,15 +125,26 @@ class JobRunner:
             self._redis.rpush("sessions", session_id)
         job_id = str(uuid4())
         self._redis.rpush(f"sessions:{session_id}", job_id)
-        self._queue.enqueue_call(
+        queue = Queue(self._queue_name, connection=self._redis, default_timeout=-1)        
+        queue.enqueue_call(
             job_id=f"{session_id}:{job_id}",
             func=operation.value,
             retry=Retry(max=3, interval=[10, 30, 60]),
             # Results are deleted when sessions are closed
             result_ttl=-1,
+            # RQ allows for both kwargs and args to be passed in, however,
+            # the `exec` interface only expects one group of arguments to
+            # be passed in.
             **{('kwargs' if isinstance(args, dict) else 'args') : args}
         )
-        return self.get_job(job_id)
+        job = self.get_job(job_id)
+        # Unfortunately, `enqueue_call` returns before job is actually in queue.
+        # Therefore, we have to poll until it makes it in there.
+        while (job is None):
+            sleep(0.5)
+            job = self.get_job(job_id)
+
+        return job
 
     
     def write_message(self, job_id: str, message: str):
@@ -165,22 +177,23 @@ class JobRunner:
 
 
         if job_id is not None:
-            _, keys = self._redis.scan(cursor=0, match=f'*:{job_id}')
-            if len(keys) != 0:
-                session_id = keys[0].decode('utf-8').split(":")[0]
-            else:
-                session_id = None
+            rq_job = None
+            candidates = [
+                job 
+                for queue in Queue.all(connection=self._redis)
+                for job in queue.jobs 
+            ] 
+            for candidate_rq_job in candidates:
+                if candidate_rq_job.id.split(":")[1] == job_id:
+                    rq_job = candidate_rq_job
+                    break
+            if rq_job is None:
+                return
         else:
-            session_id = None
+            rq_job = get_current_job()
 
-        if session_id is None:
-            session_id, job_id = get_current_job.id.split(":")
+        session_id, job_id = rq_job.id.split(":")                        
         
-        try:
-            job = rq_job.Job.fetch(job_id, connection=self._redis)
-        except NoSuchJobError:
-            return
-
         messages = [
             raw_msg.decode('utf-8')            
             for raw_msg in self._redis.lrange(f'messages:{job_id}', 0, -1)
@@ -189,14 +202,13 @@ class JobRunner:
         return Job(
             id = job_id,
             session_id = session_id,
-            status = job.get_status(),
-            result = job.return_value,
-            error = job.exc_info,
+            status = rq_job.get_status(),
+            result = rq_job.return_value,
+            error = rq_job.exc_info,
             messages = messages,
-            created_at = job.created_at,
-            enqueued_at = job.enqueued_at,
-            started_at = job.started_at,
-            redis=self._redis,
+            created_at = rq_job.created_at,
+            enqueued_at = rq_job.enqueued_at,
+            started_at = rq_job.started_at,
         )
 
 
@@ -207,7 +219,7 @@ class JobRunner:
         Args:
             job_id (str): ID of job to kill
         """
-        job = rq_job.Job.fetch(job_id, connection=self._redis)
+        job = rq_job_lib.Job.fetch(job_id, connection=self._redis)
         job.kill()
 
 
