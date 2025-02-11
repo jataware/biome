8fe54f2d35ad0bbc54f385b0c02c86f6264033f9
Matthew Printz <matthew@hipokrit.com>
2025-02-11 16:42:58 -0600

    Updates to biome app to work with latest version of beaker apps (#33)

--- src/biome/app.py
+++ src/biome/app.py
@@ -1,12 +1,10 @@
-import json
-import os
 
-from beaker_kernel.lib.app import BeakerApp, BeakerAppAsset, AppConfigStrings, Context
+from beaker_kernel.lib.app import BeakerApp
 
 
 class BiomeApp(BeakerApp):
-    SLUG = "biome"
-    ASSET_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "assets"))
+    slug = "biome"
+    name = "Biome"
 
     pages = {
         'chat': {
@@ -20,30 +18,29 @@ class BiomeApp(BeakerApp):
             "title": "Dev interface",
         }
     }
-    default_context = Context(
-        slug="biome",
-        payload={},
-    )
-    single_context = True
+    default_context = {
+        "slug": "biome",
+        "payload": {},
+        "single_context": True,
+    }
 
-    assets = [
-        BeakerAppAsset(
-            slug="header_logo",
-            src="biome-logo.png",
-            alt="Biome logo"
-        ),
-        BeakerAppAsset(
-            slug="body_logo",
-            src="biome-helix-horizontal.png",
-            alt="Biome logo"
-        )
-    ]
-    strings = AppConfigStrings(
-        short_title="~",
-        chat_welcome_html="""<div style="display: flex; flex-direction: row; align-items: center; gap: 20px;">
-          <img src="/assets/biome/biome-helix-horizontal.png" alt="Biome Logo" height="100px">
+    assets = {
+        "header_logo": {
+            "src": "biome-logo.png",
+            "alt": "Biome logo"
+        },
+        "body_logo": {
+            "src": "biome-helix-horizontal.png",
+            "alt": "Biome logo"
+        }
+    }
+
+    template_bundle = {
+        "short_title": "",
+        "chat_welcome_html": """<div style="display: flex; flex-direction: row; align-items: center; gap: 20px;">
+          <img src="{asset:body_logo:src}" alt="Biome Logo" height="100px">
           <p>Hi! I'm your Biome Agent and I can help you do all sorts of tasks related to biomedical research. I'm
-          particularly good at helping with information retrieval, data analysis, and data visualization. I have 
+          particularly good at helping with information retrieval, data analysis, and data visualization. I have
           access to a wide range of data sources and APIs including GDC, cBioPortal, and more.</p>
         </div>"""
-    )
+    }
