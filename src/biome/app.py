2ab0cd868eca43898d008faeec92e8adf6f7409f
Matthew Printz <matt@jataware.com>
2025-10-21 14:55:04 -0600

    Updates to styling

--- src/biome/app.py
+++ src/biome/app.py
@@ -4,15 +4,15 @@ from beaker_kernel.lib.app import BeakerApp
 
 class BiomeApp(BeakerApp):
     slug = "biome"
-    name = "Biome"
+    name = "Biomedical"
 
     pages = {
         "notebook": {
-            "title": "Biome notebook",
+            "title": "Beakerhub Biomedical Notebook",
             "default": True,
         },
         'chat': {
-            "title": "Biome chat",
+            "title": "Beakerhub Biomedical Chat",
         },
         "dev": {
             "title": "Dev interface",
@@ -29,20 +29,20 @@ class BiomeApp(BeakerApp):
 
     assets = {
         "header_logo": {
-            "src": "biome-logo.png",
-            "alt": "Biome logo"
+            "src": "biomedical.png",
+            "alt": "Beakerhub Biomedical"
         },
         "body_logo": {
-            "src": "biome-helix-horizontal.png",
-            "alt": "Biome logo"
+            "src": "biomedical.png",
+            "alt": "Beakerhub Biomedical"
         }
     }
 
     template_bundle = {
-        "short_title": "",
+        "short_title": "Beakerhub Biomedical Notebook",
         "chat_welcome_html": """<div style="display: flex; flex-direction: row; align-items: center; gap: 20px;">
           <img src="{asset:body_logo:src}" alt="Biome Logo" height="100px">
-          <p>Hi! I'm your Biome Agent and I can help you do all sorts of tasks related to biomedical research. I'm
+          <p>Hi! I'm your Biomedical Beaker Agent and I can help you do all sorts of tasks related to biomedical research. I'm
           particularly good at helping with information retrieval, data analysis, and data visualization. I have
           access to a wide range of data sources and APIs including GDC, cBioPortal, and more.</p>
         </div>"""
